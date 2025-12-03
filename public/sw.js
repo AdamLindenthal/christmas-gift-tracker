615300bb9e0cb256717a5d0145b9c7fd7702c318
Adam Lindenthal <adam.lindenthal@codingbear.com>
2025-12-03 17:53:05 +0100

    Add Progressive Web App support with manifest, service worker, and icons

---
+++ public/sw.js
@@ -0,0 +1,40 @@
+// Simple service worker for caching
+const CACHE_NAME = 'christmas-gifts-v1';
+const urlsToCache = [
+  '/',
+  '/login',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
